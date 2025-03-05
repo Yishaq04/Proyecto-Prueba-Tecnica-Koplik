9435c66824af596e8fc117f55c3b3576ec941c57
Yishaq04 <126306165+Yishaq04@users.noreply.github.com>
2025-03-04 23:26:46 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import node from "@astrojs/node";
+import vercel from '@astrojs/vercel/serverless'; // Adaptador para Vercel
 
 // https://astro.build/config
 export default defineConfig({
-
   vite: {
     plugins: [tailwindcss()]
   },
-  output: "server",
-  integrations: [react()],
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "server", // Indica que el proyecto se va a desplegar como servidor
+  adapter: vercel(), // Solo este adaptador
+  integrations: [react()]
 });
 
 
